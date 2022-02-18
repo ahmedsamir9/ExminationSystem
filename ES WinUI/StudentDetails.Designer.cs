7a421c1e00976590ffe6f354f6d12ea5773a4be4
AhmSaleh <ahmedsaleh166@gmail.com>
2022-02-18 18:01:58 +0200

    StudentDetails Form Added

---
+++ ES WinUI/StudentDetails.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Examination_System
+{
+    partial class StudentDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbID = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbFirstName = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbLastName = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbEmail = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbGYear = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbDeptID = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbSex = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbPassword = new MaterialSkin.Controls.MaterialTextBox();
+            this.tbUsername = new MaterialSkin.Controls.MaterialTextBox();
+            this.btnRegister = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // tbID
+            // 
+            this.tbID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbID.Depth = 0;
+            this.tbID.Enabled = false;
+            this.tbID.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbID.Hint = "Student ID";
+            this.tbID.Location = new System.Drawing.Point(45, 98);
+            this.tbID.MaxLength = 50;
+            this.tbID.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbID.Multiline = false;
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(212, 50);
+            this.tbID.TabIndex = 2;
+            this.tbID.Text = "";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbFirstName.Depth = 0;
+            this.tbFirstName.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbFirstName.Hint = "First Name";
+            this.tbFirstName.Location = new System.Drawing.Point(45, 199);
+            this.tbFirstName.MaxLength = 50;
+            this.tbFirstName.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbFirstName.Multiline = false;
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(212, 50);
+            this.tbFirstName.TabIndex = 3;
+            this.tbFirstName.Text = "";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbLastName.Depth = 0;
+            this.tbLastName.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbLastName.Hint = "Last Name";
+            this.tbLastName.Location = new System.Drawing.Point(45, 300);
+            this.tbLastName.MaxLength = 50;
+            this.tbLastName.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbLastName.Multiline = false;
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(212, 50);
+            this.tbLastName.TabIndex = 4;
+            this.tbLastName.Text = "";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbEmail.Depth = 0;
+            this.tbEmail.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbEmail.Hint = "Email";
+            this.tbEmail.Location = new System.Drawing.Point(45, 389);
+            this.tbEmail.MaxLength = 50;
+            this.tbEmail.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbEmail.Multiline = false;
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(212, 50);
+            this.tbEmail.TabIndex = 5;
+            this.tbEmail.Text = "";
+            // 
+            // tbGYear
+            // 
+            this.tbGYear.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbGYear.Depth = 0;
+            this.tbGYear.Enabled = false;
+            this.tbGYear.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbGYear.Hint = "Graduation Year";
+            this.tbGYear.Location = new System.Drawing.Point(662, 389);
+            this.tbGYear.MaxLength = 50;
+            this.tbGYear.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbGYear.Multiline = false;
+            this.tbGYear.Name = "tbGYear";
+            this.tbGYear.Size = new System.Drawing.Size(212, 50);
+            this.tbGYear.TabIndex = 9;
+            this.tbGYear.Text = "";
+            // 
+            // tbDeptID
+            // 
+            this.tbDeptID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbDeptID.Depth = 0;
+            this.tbDeptID.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbDeptID.Hint = "Departement ID";
+            this.tbDeptID.Location = new System.Drawing.Point(662, 300);
+            this.tbDeptID.MaxLength = 50;
+            this.tbDeptID.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbDeptID.Multiline = false;
+            this.tbDeptID.Name = "tbDeptID";
+            this.tbDeptID.Size = new System.Drawing.Size(212, 50);
+            this.tbDeptID.TabIndex = 8;
+            this.tbDeptID.Text = "";
+            // 
+            // tbSex
+            // 
+            this.tbSex.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbSex.Depth = 0;
+            this.tbSex.Enabled = false;
+            this.tbSex.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbSex.Hint = "Sex";
+            this.tbSex.Location = new System.Drawing.Point(662, 199);
+            this.tbSex.MaxLength = 50;
+            this.tbSex.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbSex.Multiline = false;
+            this.tbSex.Name = "tbSex";
+            this.tbSex.Size = new System.Drawing.Size(212, 50);
+            this.tbSex.TabIndex = 7;
+            this.tbSex.Text = "";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPassword.Depth = 0;
+            this.tbPassword.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbPassword.Hint = "Password";
+            this.tbPassword.Location = new System.Drawing.Point(662, 98);
+            this.tbPassword.MaxLength = 50;
+            this.tbPassword.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbPassword.Multiline = false;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Password = true;
+            this.tbPassword.Size = new System.Drawing.Size(212, 50);
+            this.tbPassword.TabIndex = 6;
+            this.tbPassword.Text = "";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbUsername.Depth = 0;
+            this.tbUsername.Font = new System.Drawing.Font("Roboto", 12F);
+            this.tbUsername.Hint = "Username";
+            this.tbUsername.Location = new System.Drawing.Point(358, 248);
+            this.tbUsername.MaxLength = 50;
+            this.tbUsername.MouseState = MaterialSkin.MouseState.OUT;
+            this.tbUsername.Multiline = false;
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(212, 50);
+            this.tbUsername.TabIndex = 10;
+            this.tbUsername.Text = "";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRegister.Depth = 0;
+            this.btnRegister.DrawShadows = true;
+            this.btnRegister.HighEmphasis = true;
+            this.btnRegister.Icon = null;
+            this.btnRegister.Location = new System.Drawing.Point(402, 468);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnRegister.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(95, 36);
+            this.btnRegister.TabIndex = 11;
+            this.btnRegister.Text = "     UPDATE      ";
+            this.btnRegister.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnRegister.UseAccentColor = false;
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
+            // 
+            // StudentDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 542);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbGYear);
+            this.Controls.Add(this.tbDeptID);
+            this.Controls.Add(this.tbSex);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.tbID);
+            this.Name = "StudentDetails";
+            this.Text = "StudentDetails";
+            this.Load += new System.EventHandler(this.StudentDetails_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTextBox tbID;
+        private MaterialSkin.Controls.MaterialTextBox tbFirstName;
+        private MaterialSkin.Controls.MaterialTextBox tbLastName;
+        private MaterialSkin.Controls.MaterialTextBox tbEmail;
+        private MaterialSkin.Controls.MaterialTextBox tbGYear;
+        private MaterialSkin.Controls.MaterialTextBox tbDeptID;
+        private MaterialSkin.Controls.MaterialTextBox tbSex;
+        private MaterialSkin.Controls.MaterialTextBox tbPassword;
+        private MaterialSkin.Controls.MaterialTextBox tbUsername;
+        private MaterialSkin.Controls.MaterialButton btnRegister;
+    }
+}
---
+++ ES WinUI/StudentDetails.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Examination_System
+{
+    public partial class StudentDetails : MaterialForm
+    {
+
+        SqlConnection sqlCn = new SqlConnection(
+            "Data Source=.;Initial Catalog=ExaminationSytem;Integrated Security=true"
+        );
+        SqlCommand sqlCmd;
+
+        public StudentDetails()
+        {
+            InitializeComponent();
+            sqlCmd = new SqlCommand();
+            sqlCmd.Connection = sqlCn;
+
+            InitForm();
+        }
+
+        private void InitForm() {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.BlueGrey800,
+                Primary.BlueGrey900,
+                Primary.BlueGrey500,
+                Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        } 
+        private void StudentDetails_Load(object sender, EventArgs e)
+        {
+            sqlCmd = new SqlCommand(
+                "SELECT * FROM Userr U INNER JOIN Student S ON U.ID = 3 AND U.id = S.stuID",
+                sqlCn
+            );
+
+            sqlCmd.Parameters.Clear();
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.CommandText = "GetStudentDEtails";
+            sqlCmd.Parameters.AddWithValue("st_id", 3); //edit 3 with the id of the current Student.
+
+
+            sqlCn.Open();
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                tbID.Text = reader["id"].ToString();
+                tbFirstName.Text = reader["firstName"].ToString();
+                tbLastName.Text = reader["lastName"].ToString();
+                tbEmail.Text = reader["email"].ToString();
+                tbUsername.Text = reader["userName"].ToString();
+                tbPassword.Text = reader["passKey"].ToString();
+                tbSex.Text = reader["sex"].ToString();
+                tbDeptID.Text = reader["depID"].ToString();
+                tbGYear.Text = reader["gradYear"].ToString();
+            }
+
+            reader.Close();
+            sqlCn.Close();
+        }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            //try {
+                sqlCmd.Parameters.Clear();
+                sqlCmd.CommandText = "UpdateUser";
+
+                sqlCmd.Parameters.AddWithValue("id", int.Parse(tbID.Text));
+                sqlCmd.Parameters.AddWithValue("firstName", tbFirstName.Text);
+                sqlCmd.Parameters.AddWithValue("lastName", tbLastName.Text);
+                sqlCmd.Parameters.AddWithValue("email", tbEmail.Text);
+                sqlCmd.Parameters.AddWithValue("userName", tbUsername.Text);
+                sqlCmd.Parameters.AddWithValue("passKey", tbPassword.Text);
+                sqlCmd.Parameters.AddWithValue("depID", int.Parse(tbDeptID.Text));
+
+                sqlCn.Open();
+                sqlCmd.ExecuteNonQuery();
+                sqlCn.Close();
+
+            //}
+            //catch (Exception Ex) {
+                //MessageBox.Show(Ex.Message.ToString());
+                //sqlCn.Close();
+            //}
+        }
+    }
+}
